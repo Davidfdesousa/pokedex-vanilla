d7f032529057e4fd6b35b3965532a47af37689ef
David Ferreira <david.ferreira@superare.co>
2021-02-10 00:17:29 -0300

    create request pokeAPI in js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
-console.log('olá, eu sou o app.js!')
+const fetchPokemon = () => {
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromises = []
+
+    for (let i =1; i <= 150; i++){
+      pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+    }
+
+    Promise.all(pokemonPromises)
+      .then(pokemons => {
+        console.log(pokemons)
+      })
+}
+
+fetchPokemon()
