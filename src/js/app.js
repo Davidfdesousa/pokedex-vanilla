9e81294bd7b5e9d250ebaeb8cce91b2e0580d6cd
David Ferreira <david.ferreira@superare.co>
2021-02-10 18:46:22 -0300

    inject html by js

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,22 @@ const fetchPokemon = () => {
 
     Promise.all(pokemonPromises)
       .then(pokemons => {
-        console.log(pokemons)
+        const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+          const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+          accumulator += `
+            <li class="card ${types[0]}">
+              <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
+              <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+              <p class="card-subtitle">${types.join(' | ')}</p>
+            </li>
+          `
+          return accumulator
+        }, '')
+
+        const ul = document.querySelector('[data-js="pokedex"]')
+
+        ul.innerHTML = lisPokemons
       })
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,2 @@
 import'../sass/main.scss'
 import'./app'
-
-console.log("olá, eu sou o index.js!")
